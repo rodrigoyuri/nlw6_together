f89fc1119044033076828302868fc823f1da6fba
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-06-30 19:16:36 -0300

    instalacao, configuracao e primeiros passos graphql

--- lib/wabanex_web/router.ex
+++ lib/wabanex_web/router.ex
@@ -8,7 +8,14 @@ defmodule WabanexWeb.Router do
   scope "/api", WabanexWeb do
     pipe_through :api
 
-    #get "/", IMCController, :index
+    # get "/", IMCController, :index
+  end
+
+  scope "/api" do
+    pipe_through :api
+
+    forward "/graphql", Absinthe.Plug, schema: WabanexWeb.Schema
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schema: WabanexWeb.Schema
   end
 
   # Enables LiveDashboard only for development
